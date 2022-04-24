2227b200a169ac466e2775aeb4e15a295278f17e
fuyunwang <2375872953@qq.com>
2022-04-24 10:48:13 +0800

    init project

---
+++ rpc_infra_netty/src/main/java/com/fuyunwang/rpc_infra_netty/RpcInfraNettyApplication.java
@@ -0,0 +1,13 @@
+package com.fuyunwang.rpc_infra_netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcInfraNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcInfraNettyApplication.class, args);
+    }
+
+}
---
+++ rpc_infra_netty/src/test/java/com/fuyunwang/rpc_infra_netty/RpcInfraNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuyunwang.rpc_infra_netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcInfraNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
